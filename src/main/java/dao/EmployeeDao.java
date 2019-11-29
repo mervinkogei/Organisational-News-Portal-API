af87b057c4858e07bd7ff76dc8cde1aaad84c76e
mervinkogei <vincent.kogei@yahoo.com>
2019-11-29 14:23:17 +0300

    Update the getters methods to EmployeeDao class

--- src/main/java/dao/EmployeeDao.java
+++ src/main/java/dao/EmployeeDao.java
@@ -1,4 +1,24 @@
 package dao;
 
+import models.Employee;
+
+import java.util.List;
+
 public interface EmployeeDao {
+    void add(Employee employee);
+
+    //List Employees
+    List<Employee>getAll();
+
+    //Find Employees By Id
+    Employee findById(int id);
+
+    //List Employees in the Department
+    List<Employee>getAllEmployeesByDepartment(int department_id);
+
+    //Delete By Id
+    void deleteById(int id);
+
+    //Clear All
+    void clearAll();
 }
