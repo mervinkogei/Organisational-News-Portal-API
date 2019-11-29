65eac5186e462142912cfab375efe66331e7b7aa
mervinkogei <vincent.kogei@yahoo.com>
2019-11-29 14:34:50 +0300

    Update the NewsDao methods that perform the CRUD operations

--- src/main/java/dao/NewsDao.java
+++ src/main/java/dao/NewsDao.java
@@ -1,4 +1,27 @@
 package dao;
 
+import models.News;
+
+import java.util.List;
+
 public interface NewsDao {
+    void add(News news);
+
+    //List News
+    List<News> getAll();
+
+    //Find By Id
+    News findById (int id);
+
+    //List News On Departmental Level
+    List<News>getAllNewsByDepartment(int department_id);
+
+    //Update News
+    void update(int id,String title, String content, String writtenBy, int department_id);
+
+    //Delete News
+    void deleteById(int id);
+
+    //Clear All News
+    void clearAll();
 }
