eeea38dc105881da5fb09c570899e021a5fc5d46
mervinkogei <vincent.kogei@yahoo.com>
2019-11-29 12:16:16 +0300

    Add methods to Department dao to perform CRUD operations

--- src/main/java/dao/DepartmentDao.java
+++ src/main/java/dao/DepartmentDao.java
@@ -1,4 +1,32 @@
 package dao;
 
+import models.Department;
+import models.Employee;
+import models.News;
+
+import java.util.List;
+
 public interface DepartmentDao {
+    void add (Department department);
+
+    // List Department
+    List<Department>getAll();
+
+    //Find Department
+    Department findById(int id);
+
+    //List Employees
+    List<Employee>getAllEmployeesForADepartment(int department_id);
+
+    //List News
+    List<News>getAllNewsForDepartment(int department_id);
+
+    //Update Department
+    void update(int id,String name,String description, int total_employees);
+
+    // Delete
+    void deleteById(int id);
+
+    //Clear
+    void clearAll();
 }
