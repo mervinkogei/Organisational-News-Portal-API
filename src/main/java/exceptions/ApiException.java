a06e79ffa70d0cd7befbd818cee5d8d09ea675c6
mervinkogei <vincent.kogei@yahoo.com>
2019-11-30 21:38:08 +0300

    update the ApiException

--- src/main/java/exceptions/ApiException.java
+++ src/main/java/exceptions/ApiException.java
@@ -1,33 +1,15 @@
 package exceptions;
 
-public class ApiException extends RuntimeException {
+public class ApiException extends  RuntimeException{
     private final int statusCode;
 
-    public ApiException(int statusCode) {
-        this.statusCode = statusCode;
-    }
-
-    public ApiException(String s, int statusCode) {
-        super(s);
-        this.statusCode = statusCode;
-    }
-
-    public ApiException(String s, Throwable throwable, int statusCode) {
-        super(s, throwable);
-        this.statusCode = statusCode;
-    }
-
-    public ApiException(Throwable throwable, int statusCode) {
-        super(throwable);
-        this.statusCode = statusCode;
-    }
-
-    public ApiException(String s, Throwable throwable, boolean b, boolean b1, int statusCode) {
-        super(s, throwable, b, b1);
+    public ApiException (int statusCode, String msg){
+        super(msg);
         this.statusCode = statusCode;
     }
 
     public int getStatusCode() {
         return statusCode;
     }
+
 }
