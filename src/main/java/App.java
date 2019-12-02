a31648e72e965cf3d8d43411e461adef554c1da2
mervinkogei <vincent.kogei@yahoo.com>
2019-12-02 12:47:58 +0300

    last commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,6 +12,20 @@ import static  spark.Spark.*;
 public class App {
 
     public static void main(String[] args) {
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+
+        // This tells our app that if Heroku sets a port for us, we need to use that port.
+        // Otherwise, if they do not, continue using port 4567.
+
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+
+        port(port);
+
         Sql2oNewsDao newsDao;
         Sql2oDepartmentDao departmentDao;
         Sql2oEmployeeDao employeeDao;
