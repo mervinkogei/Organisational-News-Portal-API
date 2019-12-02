8a58a4801c4725805f76b5bfea59444837020347
mervinkogei <vincent.kogei@yahoo.com>
2019-12-02 17:20:20 +0300

    Add the static location path to Main App

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,6 +12,8 @@ import static  spark.Spark.*;
 public class App {
 
     public static void main(String[] args) {
+        staticFileLocation("/public");
+
         ProcessBuilder process = new ProcessBuilder();
         Integer port;
 
